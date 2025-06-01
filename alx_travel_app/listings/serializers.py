989a605284fee27df7f04548ebccbb78f3a7b983
Tanson Jude Antwi <tanson.aj@gmail.com>
2025-06-01 01:04:12 +0000

    Update serializers.py

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
-
-from listings.models import Listing, Review, Booking
+from .models import Listing, Booking
 
 
 class ListingSerializer(serializers.ModelSerializer):
@@ -9,18 +8,7 @@ class ListingSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class ReviewSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Review
-        fields = '__all__'
-
-
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = '__all__'
-
-    def validate(self, data):
-        if data['start_date'] >= data['end_date']:
-            raise serializers.ValidationError("Start date must be before end date.")
-        return data
